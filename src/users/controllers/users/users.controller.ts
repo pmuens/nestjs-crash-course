9ab328eab7f392674e38c8d51e9508c8573caa05
Philipp Muens <philipp@muens.io>
2023-01-24 14:38:47 +0100

    Add GET request examples

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getUsers() {
+    return [{ username: 'jdoe', email: 'jdoe@example.com' }];
+  }
+
+  @Get('posts')
+  getUsersPost() {
+    return [
+      {
+        username: 'jdoe',
+        email: 'jdoe@example.com',
+        posts: [
+          {
+            id: 1,
+            title: 'Post 1',
+          },
+          {
+            id: 2,
+            title: 'Post 2',
+          },
+        ],
+      },
+    ];
+  }
+
+  @Get('posts/comments')
+  getUsersPostsComments() {
+    return [
+      {
+        id: 1,
+        title: 'Post 1',
+        comments: [
+          {
+            id: 1,
+            title: 'Comment 1',
+          },
+          {
+            id: 2,
+            title: 'Comment 2',
+          },
+        ],
+      },
+    ];
+  }
+}
