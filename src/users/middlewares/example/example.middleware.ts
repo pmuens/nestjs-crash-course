382fa652637fcd5254f6e00cffda8e3580100b74
Philipp Muens <philipp@muens.io>
2023-01-24 16:16:22 +0100

    Validate Authorization request header

--- src/users/middlewares/example/example.middleware.ts
+++ src/users/middlewares/example/example.middleware.ts
@@ -1,9 +1,26 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
 
 @Injectable()
 export class ExampleMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
     console.log('Example Middleware');
+
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      throw new HttpException('No Authorization Token', HttpStatus.FORBIDDEN);
+    } else if (authorization && authorization !== '12345678') {
+      throw new HttpException(
+        'Invalid Authorization Token',
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
     next();
   }
 }
